e6f7685a1e54c20dae3926eb7cbbba74f5e675cb
KangChou~OvO⁹⁹ <kangchou666@gmail.com>
2023-05-20 01:07:49 +0800

    Add files via upload

---
+++ myvue-face/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="camera">
+    <img :src="videoSource" alt="Camera Feed">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      videoSource: '',
+    };
+  },
+  mounted() {
+    this.initCamera();
+  },
+  methods: {
+    initCamera() {
+      this.videoSource = 'http://localhost:5000/video_feed';  // 更新视频源为服务器地址
+    },
+  },
+};
+</script>
+
+<style scoped>
+.camera {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f0f0f0;
+}
+.camera img {
+  max-width: 100%;
+  max-height: 100%;
+}
+</style>
---
+++ myvue-face/server.py
@@ -0,0 +1,32 @@
+from flask import Flask, Response
+import cv2
+
+app = Flask(__name__)
+
+def detect_faces():
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    video_capture = cv2.VideoCapture(0)
+
+    while True:
+        success, frame = video_capture.read()
+        if not success:
+            break
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(detect_faces(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run()
